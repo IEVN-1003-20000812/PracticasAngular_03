cef861a328e80f7a0873e6ba0742319cc2f1db7a
David Luna <eusoj.divad1@gmail.com>
2023-09-27 11:10:08 -0600

    27/09/2023 JDCL

---
+++ src/app/calculos/operas-bas/operas-bas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  templateUrl: './operas-bas.component.html',
+  styleUrls: ['./operas-bas.component.css']
+})
+export class OperasBasComponent {
+
+}
