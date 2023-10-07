2b94548662417b1a49af48ba648a3b914c495f27
David Luna <eusoj.divad1@gmail.com>
2023-10-06 19:11:40 -0600

    06/10/2023 JDCL

--- src/app/cine/cineplois/cineplois.component.ts
+++ src/app/cine/cineplois/cineplois.component.ts
@@ -8,48 +8,60 @@ import { Component } from '@angular/core';
 export class CineploisComponent {
   nombre: string = '';
   cantidadCompradores: number = 0;
-  club: boolean = false;
+  club: string='';
   cantidadBoletos: number = 0;
   maxBoletos: number = 7;
   totalPagar: number = 0;
   mostrarCamposEntrada: boolean = false;
+  error: boolean = false;
 
   CantidadBoletos() {
     if (this.cantidadCompradores <= 0) {
       this.cantidadCompradores = 1;
+      this.cantidadBoletos = 1;
     }
   
     this.maxBoletos = 7 * this.cantidadCompradores;
   
-    if (this.cantidadBoletos <= 0) {
-      this.cantidadBoletos = 1;
+    if (this.cantidadBoletos < 0) {
+      this.cantidadBoletos = 0;
     }
-
     if (this.cantidadBoletos > this.maxBoletos) {
       this.cantidadBoletos = this.maxBoletos;
     }
   }  
 
   calcularTotal() {
-    const precioPorBoleto = 12;
-    let descuento = 0;
+    if (this.cantidadBoletos > this.cantidadCompradores * 7) {
+      this.error = true;
+      this.mostrarCamposEntrada = false;
 
-    if (this.cantidadBoletos >= 5) {
-      descuento = 0.15;
-    } else if (this.cantidadBoletos >= 3) {
-      descuento = 0.10;
+    } else {
+      this.error = false;
+      const precioPorBoleto = 12;
+      let descuento = 0;
+      let descuentoclub = 0;
+  
+      if (this.cantidadBoletos >= 5) {
+        descuento = 0.15;
+      } else if (this.cantidadBoletos >= 3) {
+        descuento = 0.10;
+      }
+  
+      if (this.club === "si") {
+        descuentoclub += 0.10;
+      }
+  
+      let total = this.cantidadBoletos * precioPorBoleto;
+      total -=total * descuento;
+      total -= total * descuentoclub;
+  
+      this.totalPagar = total;
+      this.mostrarCamposEntrada = true;
     }
-
-    let total = this.cantidadBoletos * precioPorBoleto;
-    total -= total * descuento;
-
-    if (this.club) {
-      total -= total * 0.10;
-    }
-
-    this.totalPagar = total;
-    this.mostrarCamposEntrada = true;
   }
+  
+  
 
   F5() {
     window.location.reload();
